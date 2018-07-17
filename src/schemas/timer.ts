c714f29be4dc658b67501cc463310a17ad8774bf
ElRaptorus <christian.werner@5minds.de>
2018-07-17 14:13:39 +0200

    Add timer schema

---
+++ src/schemas/timer.ts
@@ -0,0 +1,45 @@
+import * as Sequelize from 'sequelize';
+
+export interface ITimerAttributes {
+  id: string;
+  timerType: number;
+  timerIsoString: string;
+  timerRule: string;
+  eventName: string;
+  lastElapsed: Date;
+}
+
+export type Timer = Sequelize.Instance<ITimerAttributes> & ITimerAttributes;
+
+export function defineTimer(sequelize: Sequelize.Sequelize): any {
+  const attributes: SequelizeAttributes<ITimerAttributes> = {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    timerType: {
+      type: Sequelize.NUMBER,
+      unique: true,
+      allowNull: false,
+    },
+    timerIsoString: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    timerRule: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    eventName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastElapsed: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  };
+
+  return sequelize.define<Timer, ITimerAttributes>('Timer', attributes);
+}
